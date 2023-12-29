bacafa7a0e595694c5daf02f005759f11aed9bfe
joel-2004 <jrjoel484@gmail.com>
2023-12-29 20:46:53 +0530

    Home.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Router } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import AdminInsert from "./components/AdminInsert";
@@ -6,9 +6,11 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Navbar></Navbar>
-        <Home></Home> */}
-        <AdminInsert></AdminInsert>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/Admin" element={<AdminInsert></AdminInsert>}></Route>
+          <Route path="/" element={<Home></Home>}></Route>
+        </Routes>
       </BrowserRouter>
     </>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,27 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios"
 const Home = () => {
+    const [list, setList] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const food = await axios.get("http://localhost:5000/getFoodData");
+                // console.log(food.data);
+                setList(food.data);
+            }
+            catch (e) {
+                console.log(e);
+            }
+        }
+        fetchData();
+    }, [list])
     return (<>
-        <h1>Home</h1>
+        {list.map((e) =>
+            <div>
+                <h1>{e.name}</h1>
+                <h1>{e.price}</h1>
+            </div>
+        )}
     </>)
 };
 
