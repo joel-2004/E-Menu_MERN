540a1c4020aae75bfc0fb2153950d45913a29e57
joel-2004 <jrjoel484@gmail.com>
2023-12-29 18:32:29 +0530

    Project Begin

---
+++ src/components/AdminInsert.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import axios from "axios"
+
+const AdminInsert = () => {
+    const [food, setFood] = useState("");
+    const [price, setPrice] = useState("");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            food: food,
+            price: price
+
+        }
+        console.log(data);
+        setFood("");
+        setPrice("");
+        try {
+            await axios.post("http://localhost:5000/insertFood", { data: data })
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+    return (
+        <>
+            <div className="container-fluid ">
+                <div className="row">
+                    <div className="col">
+                        <form onSubmit={handleSubmit}>
+                            <label htmlFor="foodName">FoodName</label>
+                            <input type="text" id="foodName" value={food} onChange={(e) => setFood(e.target.value)} ></input>
+                            <label htmlFor="price">Price</label>
+                            <input type="text" id="price" value={price} onChange={(e) => setPrice(e.target.value)}></input>
+                            <button type="submit">Add</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default AdminInsert;
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Link } from "react-router-dom"
+const Navbar = () => {
+    return (
+        <>
+            <div className="container-fluid bg-info-subtle ">
+                <div className="row">
+                    <div className="col-4"></div>
+                    <div className="col-4">
+                        <h3>   E-Menu</h3>
+                    </div>
+                    <div className="col-4 text-center">
+                        <Link>    Cart </Link>  <ShoppingCartIcon fontSize="large"></ShoppingCartIcon>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Navbar;
