ce90d6e2dcc78e432e9fba4af8373d9c2e95c7bf
joel-2004 <jrjoel484@gmail.com>
2024-01-05 23:01:41 +0530

    Updating all files

---
+++ src/components/User/Home.js
@@ -0,0 +1,11 @@
+
+import HomeUtil from "./HomeUtil";
+import NavbarUser from "./NavbarUser";
+const Home = () => {
+    return (<>
+        <NavbarUser></NavbarUser>
+        <HomeUtil></HomeUtil>
+    </>)
+};
+
+export default Home;
